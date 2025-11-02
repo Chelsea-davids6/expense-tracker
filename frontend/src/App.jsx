4f5bfe8c334a0cd3201df0b0a95bc1325c8547bd
Chelsea <132057611+Chelsea-davids6@users.noreply.github.com>
2025-11-02 22:11:07 +0200

    added log in/sign up pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
+import Login from './pages/login'
+import Signup from './pages/signup'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const navigate = useNavigate()
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to Expense Tracker</h1>
+      <button className="b1" onClick={() => navigate('/login')}>Login</button>
+      <button onClick={() => navigate('/signup')}>Sign Up</button>
+
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   )
 }
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function Login() {
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <Input class="b1">Login</Input>
+    </>
+  )
+}
+
+export default Login
